e876847cb6f75e1205a335356e00a4a3e6df20c9
EttoreBarcella <100206174+EttoreBarcella@users.noreply.github.com>
2022-06-07 10:25:48 +0200

    added main + cards & section

---
+++ src/components/Main/BaseMain.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <main class="main">
+    <BaseHero/>
+    <BaseCard/> 
+  </main>
+</template>
+
+<script>
+import BaseHero from '../Sections/BaseHero.vue';
+import BaseCard from '../Sections/BaseCard.vue';
+export default {
+    name: 'BaseMain',
+    components: { 
+      BaseHero, 
+      BaseCard
+       },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/Sections/BaseHero.vue
@@ -0,0 +1,21 @@
+
+<template>
+<section>
+
+</section>
+</template>
+
+<script>
+export default {
+    name:'BaseHero',
+}
+</script>
+
+<style lang="scss" scoped>
+section{
+    background-image: url(../../assets/img/jumbotron.jpg);
+    background-size: cover;
+    height: 400px;
+    width: 100%;
+}
+</style>
