14e3ebd92e108e268eb759d6e72e1e2b6bc4f1aa
EttoreBarcella <100206174+EttoreBarcella@users.noreply.github.com>
2022-06-07 10:20:55 +0200

    added card

---
+++ src/components/Commons/CardComics.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card">
+      <div class="image_box">
+          <img :src="image" :alt="title">
+      </div>
+    <h3>{{title}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+name: "CardComics",
+ props: {
+        title: String,
+        image: String,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.image_box{
+    height: 200px;
+    
+}
+img{
+    width: 100%;
+    height: 100%;
+    object-position: top;
+  
+}
+</style>
