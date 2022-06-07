e33d16e2c13b4a4d9081025f34a9dfbb2368c285
EttoreBarcella <100206174+EttoreBarcella@users.noreply.github.com>
2022-06-07 10:28:45 +0200

    added footer top

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <BaseHeader/>
     <BaseMain/>
+    <FooterTop/>
   </div>
 </template>
 
 <script>
 import BaseHeader from './components/Header/BaseHeader.vue'
 import BaseMain from './components/Main/BaseMain.vue'
+import FooterTop from './components/Footer/FooterTop.vue'
 
 export default {
   name: 'App',
   components: {
     BaseHeader,
     BaseMain,
+    FooterTop,
   }
 }
 </script>
---
+++ src/components/Footer/FooterTop.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="footer-top">
+      <ul class="footer-top__nav">
+        <li><a href="#"><img src="../../assets/img/buy-comics-digital-comics.png" alt=""> digital comics</a></li>
+        <li><a href="#"><img src="../../assets/img/buy-comics-merchandise.png" alt=""> dc merchandise</a></li>
+        <li><a href="#"><img src="../../assets/img/buy-comics-subscriptions.png" alt=""> subscription</a></li>
+        <li><a href="#"><img src="../../assets/img/buy-comics-shop-locator.png" alt=""> comic shop locator</a></li>
+        <li><a href="#"><img src="../../assets/img/buy-dc-power-visa.svg" alt=""> dc power visa</a></li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'FooterTop'
+}
+</script>
+
+<style lang="scss" scoped>
+.footer-top{
+  padding: 50px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--primary-color);
+    
+  &__nav{
+    display: flex;
+    align-items: center;
+    list-style: none;
+    gap: 1.875rem;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 3.75rem;
+    a{
+      display: flex;
+      align-items: center;
+      gap: 1.25rem;
+      color: white;
+      text-decoration: none;
+      text-transform: uppercase;
+      font-size: .75rem;
+    }
+  }
+  img{
+    width: 30px;
+  }
+ 
+}
+</style>
